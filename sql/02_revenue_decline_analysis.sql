72e157c30debb643e99770bbe2bf19facf24d41f
Your New Name <your.new.email@example.com>
2026-06-15 01:10:19 +0500

    Updated sql analysis and added a market analysis notebook

--- sql/02_revenue_decline_analysis.sql
+++ sql/02_revenue_decline_analysis.sql
@@ -18,7 +18,6 @@ GROUP BY year
 ORDER BY year;  	--  Revenue in 2018 -> 41.4 Cr, decline after that in 2019 and 2020 (33.6Cr, 14.2Cr)
 -- 2017 can not be comprehended because it has data for only last 3 months. 
 
-/* Identify if 2020 is expected to see growth in last 6 months to overcome decline */
 SELECT *,
 	SUM(revenue) OVER(ORDER BY year,qtr) AS rolling_total,
     DENSE_RANK() OVER(PARTITION BY year ORDER BY revenue DESC) AS revenue_contribution_rank
@@ -55,6 +54,8 @@ FROM (
 ORDER BY product_type, year, qtr;
 -- Q4 ranks last in revenue contribution for both product types Distribution and Own Brand, except for Distribution in 2019.
 -- only Products codes available no Product names or categories. Thus, it cannot be identified if a certain product or category is failing in Q4.
+
+-- CUSTOMER BASED ANALYSIS
 
 -- Customer type breakdown
 SELECT *,
@@ -140,9 +141,7 @@ ORDER BY customer_type, custmer_name, year, qtr;
 12/19 E-commerce Customers buy less in Q4 across both years
 */
 
-
 --  Quantify how much does each customers effect the Revenue
-
 WITH cust_revenue AS (
 	SELECT customer_type,
 			custmer_name,
@@ -174,5 +173,176 @@ Control, Surge Stores, Acclaimed Stores, Forward Stores, Epic Stores, Nomad Stor
 -> Q4 Revenue decline is concentrated among major customers.
 */
 
+-- MARKET BASED ANALYSIS
+
+WITH market_rev AS (
+	SELECT zone, 
+		SUM(sales_amount) AS revenue
+    FROM markets_clean AS mc 
+	INNER JOIN transactions_clean AS tc 
+		ON mc.markets_code = tc.market_code
+	GROUP BY zone
+)
+SELECT *,
+	ROUND((revenue*100) / SUM(revenue) OVER(),1) AS Revenue_share
+FROM market_rev;	-- North holds 68.6% Revenue Share, Central - 26.7% and South - 4.6%
+-- Revenue is significantly concentrated in the North zone
+
+SELECT zone, COUNT( DISTINCT markets_name) AS total_markets
+FROM transactions_clean AS tc
+INNER JOIN markets_clean AS mc
+	ON tc.market_code = mc.markets_code
+GROUP BY zone;		-- 6 markets in North, 5 in South and 3 in central
+
+SELECT zone,
+	YEAR(order_date) AS year,
+    SUM(sales_amount) AS revenue
+FROM transactions_clean AS tc
+INNER JOIN markets_clean AS mc
+	ON tc.market_code = mc.markets_code
+GROUP BY zone, year
+ORDER BY zone, year;
+-- 2018-2019: Revenue decline of 6.2Cr in North zone and 1.3Cr in Central zone
+
+SELECT markets_code,
+	markets_name,
+    SUM(sales_amount) AS revenue
+FROM transactions_clean AS tc
+INNER JOIN markets_clean AS mc
+	ON tc.market_code = mc.markets_code
+WHERE zone = 'North'
+GROUP BY markets_code, markets_name
+ORDER BY revenue DESC; -- Delhi NCR is the biggest market with revenue 52Cr across all years
+-- Ahmedabad - 13Cr, Kanpur 1.3Cr and the rest of markets are below 50lakhs
+
+SELECT markets_code,
+	markets_name,
+    SUM(sales_amount) AS revenue
+FROM transactions_clean AS tc
+INNER JOIN markets_clean AS mc
+	ON tc.market_code = mc.markets_code
+WHERE zone = 'Central'
+GROUP BY markets_code, markets_name
+ORDER BY revenue DESC; -- Mumbai is the biggest market with revenue 15Cr across all years
+-- Bhopal has two markets with combined revenue of - 5.8Cr, Nagpur - 5.5Cr
+
+SELECT markets_code,
+	markets_name,
+    SUM(sales_amount) AS revenue
+FROM transactions_clean AS tc
+INNER JOIN markets_clean AS mc
+	ON tc.market_code = mc.markets_code
+WHERE zone = 'South'
+GROUP BY markets_code, markets_name
+ORDER BY revenue DESC; -- Only 2 Markets above 1.8Cr (Kochi & Chennai), Hyderabad - 74 Lakhs, rest are all below 9Lakhs
+
+SELECT *,
+	revenue_19 - revenue_18 AS revenue_change
+FROM (
+	SELECT zone, markets_code, markets_name,
+		   SUM(CASE WHEN YEAR(order_date)=2018 THEN sales_amount ELSE 0 END) AS revenue_18,
+		   SUM(CASE WHEN YEAR(order_date)=2019 THEN sales_amount ELSE 0 END) AS revenue_19
+	FROM transactions_clean tc
+	JOIN markets_clean mc ON tc.market_code = mc.markets_code
+	GROUP BY zone, markets_code, markets_name
+    ) AS t
+ORDER BY zone, revenue_change;
+/* 2018-2019: 
+North:
+- all markets are declining
+- Delhi NCR - 5Cr, Ahmedabad - 89lakhs,  Kanpur - 14Lakhs, and the last 3 combined - around 25lakhs 
+Central:
+- 2018-2019: Bhopal(Mark007) sees growth of 12lakhs, rest are all declining
+- Mumbai - 1.1Cr, Bhopal(Mark013) just under 10lakhs, Nagpur - 17Lakhs
+South:
+- Growth in 3 markets, Hyderabad & Kochi - over 3.5lakhs, Bhubaneshwar - 1.3Lakh. Chennai saw decline of 38Lakhs
+- Bengaluru market shut down in 2018 ( 0 revenue in 2019)
+*/
+
+SELECT zone,
+	COUNT(*) AS order_volume,
+	ROUND(AVG(sales_amount),1) AS avg_order_value,
+    AVG(sales_qty) AS avg_basket_size
+FROM transactions_clean AS tc
+INNER JOIN markets_clean AS mc
+	ON tc.market_code = mc.markets_code
+INNER JOIN customers AS cust
+	ON tc.customer_code = cust.customer_code 
+GROUP BY zone;	-- Central leads in order volume 72k, North 68k, South 8k
+-- North has the highest average order value of 9.9k, South - 5.6k, Central - 3.6k
+-- South has the highest avg basket size of 49, North - 18, Central - 10
+
+WITH stats_2018 AS (
+	SELECT zone,
+		YEAR(order_date) AS year,
+		COUNT(*) AS order_volume_18,
+		ROUND(AVG(sales_amount),1) AS avg_order_value_18,
+		AVG(sales_qty) AS avg_basket_size_18
+	FROM transactions_clean AS tc
+	INNER JOIN markets_clean AS mc
+		ON tc.market_code = mc.markets_code
+	GROUP BY zone, year
+	HAVING year = 2018
+	ORDER BY zone
+    ),
+stats_2019 AS (
+	SELECT zone,
+		YEAR(order_date) AS year,
+		COUNT(*) AS order_volume_19,
+		ROUND(AVG(sales_amount),1) AS avg_order_value_19,
+		AVG(sales_qty) AS avg_basket_size_19
+	FROM transactions_clean AS tc
+	INNER JOIN markets_clean AS mc
+		ON tc.market_code = mc.markets_code
+	GROUP BY zone, year
+	HAVING year = 2019
+	ORDER BY zone
+    )
+SELECT st18.zone,
+    order_volume_19 - order_volume_18 AS order_vol_change,
+    avg_order_value_19 - avg_order_value_18 AS avg_order_val_change,
+    avg_basket_size_19 - avg_basket_size_18 AS avg_basket_size_change
+FROM stats_2018 AS st18
+INNER JOIN stats_2019 AS st19
+	ON st18.zone = st19.zone;
+-- Central: All 3 decreased 
+-- North and South: Decrease in order_volume, increase in avg_order_val and avg_basket_size
 
 
+-- Customers and zones commbined
+SELECT zone, COUNT(DISTINCT custmer_name) AS total_customers
+FROM transactions_clean AS tc
+INNER JOIN markets_clean AS mc
+	ON tc.market_code = mc.markets_code
+INNER JOIN customers AS cust
+	ON tc.customer_code = cust.customer_code
+GROUP BY zone;
+-- Central has the most customers - 36, North - 26, South - 11
+
+SELECT custmer_name, COUNT(DISTINCT zone) AS zone_count
+FROM transactions_clean AS tc
+INNER JOIN markets_clean AS mc
+	ON tc.market_code = mc.markets_code
+INNER JOIN customers AS cust
+	ON tc.customer_code = cust.customer_code 
+GROUP BY custmer_name
+ORDER BY custmer_name;
+-- Most of the customers exist in multiple zones (purchases from multiple zones)
+
+SELECT zone_count, COUNT(custmer_name) AS customer_count
+FROM (
+	SELECT custmer_name, 
+		COUNT(DISTINCT zone) AS zone_count
+	FROM transactions_clean AS tc
+	INNER JOIN markets_clean AS mc
+		ON tc.market_code = mc.markets_code
+	INNER JOIN customers AS cust
+		ON tc.customer_code = cust.customer_code 
+	GROUP BY custmer_name
+	ORDER BY custmer_name
+    ) AS t
+GROUP BY zone_count;
+-- 19 customers make purchases from 2 zones, 11 customers are only limited to a single zone, 8 customers exist across all zones
+
+/* South is a new developing zone with growth potential - revenue growth in 3/4 markets, good avg order value, highest avg basket size.
+However what lacks is, developed customer base only 11 customers in the whole zone and order volume of only 8110. */ 
